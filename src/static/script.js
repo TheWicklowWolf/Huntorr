62a806dd6bbe82fc8b27f83a8ac82fd4a70ec258
TheWicklowWolf <111055425+TheWicklowWolf@users.noreply.github.com>
2023-09-26 15:20:59 +0100

    Add files via upload

---
+++ src/static/script.js
@@ -0,0 +1,106 @@
+var searchButton = document.getElementById('searchButton');
+var searchText = document.getElementById('searchText');
+var choiceButton = document.getElementById("choiceButton");
+var choiceNo = document.getElementById("choiceNo");
+var tableDiv = document.getElementById('tableDiv');
+var Radio1377X = document.getElementById('Radio1377X');
+var RadioEZTV = document.getElementById('RadioEZTV');
+var RadioPB = document.getElementById('RadioPB');
+var selectionGroup = document.getElementById('selectionGroup');
+var engineText = "";
+
+searchButton.addEventListener('click', function () {
+    // Disable Buttons
+    searchButton.disabled = true;
+    searchText.disabled = true;
+    choiceButton.disabled = true;
+    choiceNo.disabled = true;
+    Radio1377X.disabled = true;
+    RadioEZTV.disabled = true;
+    RadioPB.disabled = true;
+    // Get selected radio button
+    if (document.getElementById('Radio1377X').checked) {
+        engineText = '1377X';
+    }
+    else if (document.getElementById('RadioEZTV').checked) {
+        engineText = 'EZTV';
+    }
+    else if (document.getElementById('RadioPB').checked) {
+        engineText = 'PB';
+    }
+    fetch('/', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({ input: searchText.value, engine: engineText })
+    })
+        .then(function (response) {
+            if (response.ok) {
+                response.json()
+                    .then(function (response) {
+                        selectionGroup.style.display = "flex";
+                        // Delete exisiting table
+                        while (tableDiv.firstChild) {
+                            tableDiv.removeChild(tableDiv.firstChild);
+                        }
+                        // Insert into table
+                        tableDiv.insertAdjacentHTML('beforeend', response["table"]);
+                        var actualTable = document.getElementsByClassName("dataframe tableStyle");
+
+                        actualTable[0].addEventListener('click', function (e) {
+                            var selectedRow = e.target.parentElement.rowIndex - 1;
+                            if (selectedRow >= 0 && !isNaN(selectedRow)) {
+                                choiceNo.value = selectedRow;
+                            }
+                        });
+                    });
+            }
+            else {
+                throw Error('Something went wrong');
+            }
+            // Enable Buttons
+            searchButton.disabled = false;
+            searchText.disabled = false;
+            choiceButton.disabled = false;
+            choiceNo.disabled = false;
+            Radio1377X.disabled = false;
+            RadioEZTV.disabled = false;
+            RadioPB.disabled = false;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+});
+
+choiceButton.addEventListener('click', function () {
+    if (choiceNo.value.length == 0 || isNaN(choiceNo.value) || choiceNo.value < 0) {
+        alert("Incorrect Entry\nEnter a number to download");
+    }
+    else {
+        choiceButton.disabled = true;
+        fetch('/', {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({ "choice": choiceNo.value })
+        })
+            .then(function (response) {
+                if (response.ok) {
+                    response.json()
+                        .then(function (response) {
+                            choiceButton.disabled = false;
+                            console.log(response)
+                        });
+                }
+                else {
+                    throw Error('Something went wrong');
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+});
